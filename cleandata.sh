7f7ad098877ab74e6af40b46ae15a8babbe475e5
eccervantes <70276984+eccervantes@users.noreply.github.com>
2022-03-31 09:37:15 -0600

    Update cleandata.sh

--- cleandata.sh
+++ cleandata.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 #for i in $(cut -f 1  $1) ; do echo  "$i": \"false\", ; done
+echo "original lines: wc -l $1"
+echo ""
 for i in $(grep -v "Contract not found" $1 | cut -f 1); do echo  \"name:$i\": \"false\", ; done
 echo ""
 echo "total Tenants: $(grep -v "Contract not found" $1|wc -l )"
