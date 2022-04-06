979cc9347cd8d7f87cc1ef7d6f070c3fc6723804
juan-llamas <95446221+juan-llamas@users.noreply.github.com>
2022-04-06 11:42:33 -0500

    Update cleandata.sh

--- cleandata.sh
+++ cleandata.sh
@@ -3,7 +3,7 @@
 #for i in $(cut -f 1  $1) ; do echo  "$i": \"false\", ; done
 echo "original lines: $(wc -l $1)"
 echo ""
-for i in $(grep -v "Contract not found" $1 | cut -f 1); do echo  \"name:$i\": \"false\", ; done
+for i in $(grep -v "Contract not found" $1 | cut -f 1); do echo -e '\t\t' \"name:$i\": \"false\", ; done
 echo ""
 echo "total Tenants: $(grep -v "Contract not found" $1|wc -l )"
 
