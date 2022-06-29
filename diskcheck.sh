05537fe917107cd967ed1a00d08f2419a5797f97
juan-llamas <95446221+juan-llamas@users.noreply.github.com>
2022-06-29 10:02:22 -0500

    Add files via upload

---
+++ diskcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+server="vm-healthcheck"
+tenant=$1
+token=$(cat token)
+
+    ./rebootVM.sh $tenant $server
+    sleep 30
+    disk=$(curl -s -X GET "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/$tenant/vminstances/$server/disks" -H "accept: application/json" -H "authorization: Bearer $token")
+    echo "$server rebooted. E drive mounted:"
+    echo $disk | jq . 
---
+++ rebootVM.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+server=$2
+tenant=$1
+token=$(cat token)
+
+currentStatus=$(curl -s -X GET "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/$tenant/vminstances/$server" -H "accept: application/json" -H "authorization: Bearer $token" | jq .status | sed 's/"//g') 
+
+
+    if [ "$currentStatus" != "TERMINATED" ]; then
+        echo "---VM is not TERMINATED---"
+        echo "Preparing for stopping VM..."
+        curl -s -X POST "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/$tenant/vminstances/$server/stop" -H "accept: text/plain" -H "authorization: Bearer $token"
+        verify=$currentStatus
+        while [ "$verify" != "TERMINATED" ]
+        do
+            echo "Awating for VM server $server to be Shutdown in tenant: $tenant"
+            sleep 30
+            verify=$(curl -s -X GET "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/$tenant/vminstances/$server" -H "accept: application/json" -H "authorization: Bearer $token" | jq .status | sed 's/"//g') 
+        done
+        sleep 10
+        echo "Waiting to Start VM..."
+        curl -s -X POST "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/$tenant/vminstances/$server/start" -H "accept: text/plain" -H "authorization: Bearer $token"
+        verify=$(curl -s -X GET "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/$tenant/vminstances/$server" -H "accept: application/json" -H "authorization: Bearer $token" | jq .status | sed 's/"//g') 
+        while [ "$verify" != "RUNNING" ]
+        do
+            echo "Awating for VM server $server to be PowerOn in tenant: $tenant"
+            sleep 30
+            verify=$(curl -s -X GET "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/$tenant/vminstances/$server" -H "accept: application/json" -H "authorization: Bearer $token" | jq .status | sed 's/"//g') 
+        done
+
+    elif [ "$currentStatus" == "TERMINATED" ]; then
+        curl -s -X POST "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/$tenant/vminstances/$server/start" -H "accept: text/plain" -H "authorization: Bearer $token"
+        verify=$currentStatus
+        while [ "$verify" != "RUNNING" ] 
+        do
+            echo "Awating for VM server $server to be PowerOn in tenant: $tenant"
+            sleep 30
+            verify=$(curl -s -X GET "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/$tenant/vminstances/$server" -H "accept: application/json" -H "authorization: Bearer $token" | jq .status | sed 's/"//g') 
+        done
+    fi
