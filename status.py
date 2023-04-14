6832de88d40c1cbaedbcae0d643ecf9dec643b06
juan-llamas <95446221+juan-llamas@users.noreply.github.com>
2023-04-14 11:14:29 -0600

    Update status.py
    Added format and check for deployment servers

--- status.py
+++ status.py
@@ -13,36 +13,40 @@ def cmdline(command):
     )
     return process.communicate()[0]
 
+def banner(message, border='-'):
+    line = border * (len(message)-15)
+    print(line)
+    print(message)
+    print(line)
 
 def main(argv):  
     argv = sys.argv[1:]
-
     if (len(argv)-1) < 1:
         tenant = argv[0]
         server = 'vm-healthcheck'
     else:
         tenant = argv[0]
         server = argv[1]
-
     token = str(cmdline("gcloud auth print-access-token").decode( "utf-8" ).strip())
     headers = {"Authorization": "Bearer " + token}
     url_status = "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/" + tenant + "/vminstances/" + server
-    
-    print("Press ctrl-c to stop")
+    print("Press ctrl-c to stop\n")
     loop_forever = True
     while loop_forever:
         try:
             current_status = requests.get(url_status, headers=headers)
             if  "message" in current_status.json():
-                print (f'Error: {current_status.json()["message"]}\n')
+                banner(f'Error: {current_status.json()["message"]}')
+                time.sleep(10)
+            elif current_status.json()["name"] == 'ad-server' or current_status.json()["name"] == 'admirror-server' or current_status.json()["name"] == 'shared-storage' or current_status.json()["name"] == 'seismic-storage' or current_status.json()["name"] == 'license-server':
+                banner(f'VM name: {current_status.json()["name"]}\nStatus: {current_status.json()["status"]}')
                 time.sleep(10)
             elif "operationProgress" in current_status.json():
-                print (f'VM name: {current_status.json()["name"]}\nStatus: {current_status.json()["status"]}\nProgress: {current_status.json()["operationProgress"]}%\n')
+                banner(f'VM name: {current_status.json()["name"]}\nStatus: {current_status.json()["status"]}\nProgress: {current_status.json()["operationProgress"]}%')
                 time.sleep(10)
             else:
-                print (f'VM name: {current_status.json()["name"]}\nStatus: {current_status.json()["status"]}\nOperation: {current_status.json()["operation"]}\n')
+                banner(f'VM name: {current_status.json()["name"]}\nStatus: {current_status.json()["status"]}\nOperation: {current_status.json()["operation"]}')
                 time.sleep(10)
-
         except KeyboardInterrupt:
             loop_forever = False
     print('Status execution exit()')    
