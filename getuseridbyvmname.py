121bf976513e6d15f8b3a47b0518fb8bbf75258d
juan-llamas <95446221+juan-llamas@users.noreply.github.com>
2023-07-03 11:23:00 -0600

    Update getuseridbyvmname.py
    searching for email id

--- getuseridbyvmname.py
+++ getuseridbyvmname.py
@@ -3,33 +3,43 @@ import sys
 import requests
 import os
 import json
-import csv
-import operator
+import re
 
 def main(argv):
     tenant = sys.argv[1]
-    vmname = sys.argv[2]
+    vmname_user = sys.argv[2]
     token = os.popen("gcloud auth print-access-token").read().strip()
     headers = {"Authorization": "Bearer " + token}
     nextpagetoken = ""
     validation = 0
-    #getusers_url = "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/" + tenant + "/users?nextPageToken=" + nextpagetoken
-    print(f'searching for UserID owner of  {vmname} on Tenant {tenant}. . .  ')
-    while validation == 0: 
+    x = re.search('^vm-', vmname_user)
+    print(f'searching for UserID owner of  {vmname_user} on Tenant {tenant}. . .  ')
+    if(x!=None): 
+        while validation == 0: 
+                try:
+
+                    getusers_url = "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/" + tenant + "/users?nextPageToken=" + nextpagetoken
+                    getuser = requests.get(getusers_url, headers=headers,).json()
+                except requests.exceptions.ReadTimeout: 
+                    pass 
+                for i in getuser['users']:
+                    if 'vmInstanceName' in i and i['vmInstanceName'] == vmname_user:
+                        print("===== Found UserID =====")
+                        print(i['vmInstanceName'])
+                        print(i['email'])
+                        validation=1
+                nextpagetoken = getuser['nextPageToken']
+    else:
         try:
-            getusers_url = "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/" + tenant + "/users?nextPageToken=" + nextpagetoken
-            getuser = requests.get(getusers_url, headers=headers,).json()
-        except requests.exceptions.ReadTimeout: 
-            pass
-       
-        for i in getuser['users']:
-            if 'vmInstanceName' in i and i['vmInstanceName'] == vmname:
-                print("===== Found UserID =====")
-                print(i['vmInstanceName'])
-                print(i['email'])
-                validation=1
-        nextpagetoken = getuser['nextPageToken']
-        #print(nextpagetoken)
+            getvm_url = "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/" + tenant + "/users/" + vmname_user
+            getvm = requests.get(getvm_url, headers=headers,).json()
+            print("===== Found VM =====")
+            print(getvm['vmInstanceName'])
+            print(getvm['email'])
+        except Exception:
+            error = f'{vmname_user + ", VM not found"}'
+            print(f'{error}')
+
 
 if __name__ == "__main__":
    main(sys.argv)
