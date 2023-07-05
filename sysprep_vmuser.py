23d61a410904104f9b397e8b757fd66b7e198bf3
Ernesto <ernestocervantescervantes@hotmail.com>
2023-07-05 10:04:48 -0600

    script modified to use file and tenant with vmname

--- sysprep_vmuser.py
+++ sysprep_vmuser.py
@@ -7,20 +7,16 @@ import json
 import re
 import requests
 
-def banner(message, border='-'):
-    line = border * (len(message)-15)
-    print(line)
-    print(message)
-    print(line)
 
 def main(argv):  
     argv = sys.argv[1:]
-    file = open(argv[0], 'r')
+    tenants = []
     if (len(argv)-1) < 1:
+        file = open(argv[0], 'r')
         tenants = file.readlines()
         server = 'vm-healthcheck'
     else:
-        tenants = file.readlines()
+        tenants.append(argv[0])
         server = argv[1]
     token = os.popen("gcloud auth print-access-token").read().strip()
     headers = {"Authorization": "Bearer " + token}
@@ -31,7 +27,7 @@ def main(argv):
         url_log = "https://p-pfs-slb-1-1bgapjz.uc.r.appspot.com/api/v1/projects/"+ tenant +"/vminstances/" + server + "/log?alt=json" 
         log = requests.get(url_log, headers=headers).json()
         if  "message" in log:
-            print({tenant},{server},{log['message']})
+            print(f'{tenant},{server},{log["message"]}')
         elif log['content'] != "":
             x = re.findall(sysprep, log['content'])
             print(f'{tenant},{server},{" ".join(x)}')
