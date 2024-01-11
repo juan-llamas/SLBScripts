acc9f9d07c76d67b23bad967cbfc842b9a339e04
juan-llamas <95446221+juan-llamas@users.noreply.github.com>
2024-01-11 13:49:31 -0600

    Update report.py
    code funcitons

--- report.py
+++ report.py
@@ -5,33 +5,40 @@ import csv
 import sys
 from datetime import datetime
 
-def main(argv):
-    file_in= sys.argv[1]
-    file_out= "Format-" + file_in 
+def read_csv(file_in):
     data = pd.read_csv(file_in)
+    return data
+
+def add_status_column(data):
     status = pd.DataFrame(['']*len(data))
     data.insert(1, 'Status', status)
+    return data
 
+def format_timestamp(data):
     timestamp = data['timestamp']
-    project = data['jsonPayload.project']
-    engagement = data['jsonPayload.engagementType']
-    instance = data['jsonPayload.instance']
-    status = data['Status']    
-    instanceStatus = data['jsonPayload.instanceStatus']
-    watchdog = data['jsonPayload.watchdogOperation']
-    msg = data['jsonPayload.msg']
-    message = data['jsonPayload.message']
+    formatted_timestamp = (pd.to_datetime(timestamp)).dt.strftime('%e-%m-%Y %r')
+    data['timestamp'] = formatted_timestamp
+    return data
 
-    timestamp = (pd.to_datetime(timestamp)).dt.strftime('%e-%m-%EY %r')
-
-    rows = zip(timestamp, project, engagement, instance, status, instanceStatus, watchdog, msg, message)
-  
+def write_to_csv(data, file_out):
     headers = ['DateTime', 'Tenant', 'Engagement Type', 'VM', 'Status', 'VM Status', 'Operation', 'Message', 'Error']
     with open(file_out, 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(headers)
+        rows = zip(data['timestamp'], data['jsonPayload.project'], data['jsonPayload.engagementType'],
+                   data['jsonPayload.instance'], data['Status'], data['jsonPayload.instanceStatus'],
+                   data['jsonPayload.watchdogOperation'], data['jsonPayload.msg'], data['jsonPayload.message'])
         for row in rows:
             writer.writerow(row)
 
+def main(argv):
+    file_in = sys.argv[1]
+    file_out = "Format-" + file_in
+
+    data = read_csv(file_in)
+    data = add_status_column(data)
+    data = format_timestamp(data)
+    write_to_csv(data, file_out)
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
